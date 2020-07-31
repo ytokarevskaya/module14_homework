b419236bf2f6e1099ed4c8df1ab13f4e3f963e29
ytokarevskaya <lan2214@yandex.ru>
2020-07-31 11:39:40 +0300

    Проверено

--- task04.js
+++ task04.js
@@ -2,10 +2,10 @@ const submitBtn = document.querySelector("button");
 const img = document.getElementById("urlImg");
 
 submitBtn.addEventListener("click", () => {
-  let firstNum = document.getElementById("firstNum").value;
-  let secondNum = document.getElementById("secondNum").value;
-  if (typeof +firstNum && typeof +secondNum === "number" && !isNaN(+firstNum) && !isNaN(+secondNum)) {
-    if (+firstNum >= 150 && +firstNum <= 300 && +secondNum >= 150 && +secondNum <= 300) {
+  let firstNum = +document.getElementById("firstNum").value;
+  let secondNum = +document.getElementById("secondNum").value;
+  if (typeof firstNum === "number" && typeof secondNum === "number" && !isNaN(firstNum) && !isNaN(secondNum)) {
+    if (firstNum >= 150 && firstNum <= 300 && secondNum >= 150 && secondNum <= 300) {
       let fetchUrl = `https://picsum.photos/${firstNum}/${secondNum}`;
       fetch(fetchUrl)
         .then((response) => {
@@ -20,3 +20,7 @@ submitBtn.addEventListener("click", () => {
     console.log("одно из чисел вне диапазона от 100 до 300");
   }
 })
+
+// Задание выполнено верно, но есть пару мелких замечаний:
+// 1. В первой проверке на typeof забыли дописать === "number"
+// 2. Чтобы каждый раз не писать унарный плюс, можно привести значение сразу при присваивании (выше исправила)
